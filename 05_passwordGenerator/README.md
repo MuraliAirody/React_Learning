297d6762e312469b2a7a8bb91ae9d2f90b2094e9
MuraliAirody <muraliairody@gmail.com>
2023-11-23 17:23:09 +0530

    password generator

---
+++ 05_passwordGenerator/README.md
@@ -0,0 +1,59 @@
+# Password Generator Application
+
+# Text Converter
+
+created project using 
+```node
+   npm create vite@latest
+```
+
+to run the App use
+
+```node
+   npm run dev
+```
+
+### **[Live Demo](https://replit.com/@MuraliAirody/Text-Converter)**
+
+# BootStrap v5
+Copy-paste the stylesheet \<link> into your \<head> before all other stylesheets to load our CSS.
+
+
+```html
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+```
+Many of our components require the use of JavaScript to function. Specifically, they require our own JavaScript plugins and Popper. Place one of the following \<script> s near the end of your pages, right before the closing \</body> tag, to enable them.
+
+```html
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+```
+
+# React Icons
+
+```javascript
+ npm install react-icons --save
+
+```
+check out the bellow link for different icons
+### **[react icons](https://react-icons.github.io/react-icons/)**
+
+
+# Material UI
+
+```js
+npm install @mui/material @emotion/react @emotion/styled
+```
+Check the bellow doc for the usage of material ui
+### **[material ui](https://mui.com/material-ui/getting-started/)**
+
+### callBack Hook and useEffect Hook
+
+```text
+useCallback and useEffect are two different hooks in React, and their dependencies work in different ways1:
+
+useCallback: This hook allows you to memoize a function and pass it to child components without causing unnecessary re-renders. The dependencies array is used to determine when the function should be re-memoized. If any of the dependencies change, the function is re-memoized and a new reference is returned. You should use useCallback when you need to pass a function down to a child component as a prop, but the function may be expensive to compute or causes unnecessary re-renders.
+
+useEffect: This hook allows you to perform side effects, such as retrieving data from an API, updating the DOM, or subscribing to events. useEffect will run the function inside when the dependency array changes1. It’s the alternative for the class component lifecycle methods componentDidMount, componentWillUnmount, componentDidUpdate, etc.
+
+In summary, useCallback will create a new function when the dependency array changes, while useEffect will run the function inside when the dependency array changes. Functions with useCallback and values with useMemo can be used as dependency in useCallback, useMemo and useEffect.
+```
---
+++ 05_passwordGenerator/src/App.jsx
@@ -0,0 +1,83 @@
+import { useState, useEffect } from 'react'
+import { MdOutlineRestartAlt } from "react-icons/md";
+import {Alert} from "@mui/material"
+
+function App() {
+
+  const [length, setLength] = useState(6)
+  const [numberValid, setNumberValid] = useState(false)
+  const [charValid, setCharValid] = useState(false)
+  const [password, setPassword] = useState("")
+  const [openAlert,setOpenAlert] = useState(false)
+
+  const generatePassword = () => {
+    let pass = ""
+    let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+
+    if (charValid) {
+      str += "`!@#$%^&*()_-+=/*?+\"\'\\;\/<>,.:{}[]"
+    }
+    if (numberValid) {
+      str += "0123456789"
+    }
+
+    for (let i = 1; i <= length; i++) {
+      let random = Math.floor(Math.random() * str.length + 1)
+      pass += str[random]
+    }
+    setPassword(pass)
+  }
+
+  useEffect(() => {
+    generatePassword()
+  }, [length, numberValid, charValid])
+
+  const copyPasswordToClipBoard = ()=>{
+    window.navigator.clipboard.writeText(password)
+    setOpenAlert(true)
+
+    setTimeout(()=>{
+      setOpenAlert(false)
+    },2000)
+  }
+
+  const handleReset = () => {
+    generatePassword()
+  }
+  return (
+    <>
+      <div className='container bg-secondary w-80' style={{ margin: '100px auto', padding: '20px', borderRadius: '30px' }}>
+        <div className="d-flex flex-column align-items-center gap-4 w-100 p-5">
+           <h1>Password Generator</h1>
+           
+           {openAlert && <Alert severity="success">Copied to clipboard!</Alert>}
+          <div className="input-group mb-3">
+            <input value={password} type="text" readOnly className="form-control rounded-pill " placeholder="Password" />
+            <button onClick={copyPasswordToClipBoard} className="btn btn-primary rounded-pill " type="button">Copy</button>
+          </div>
+
+          <div className='d-flex gap-4 align-items-center w-100  p-0'>
+            <div className="input-group flex-nowrap">
+            <label htmlFor="">Length({length})</label>
+              <input type="range" style={{ cursor: 'pointer' }} min={6} max={50} value={length} name="length" onChange={(e) => { setLength(e.target.value) }} />
+            </div>
+            <div className="input-group ">
+              <input type="checkbox" name="Character" id="" onChange={(e) => { setNumberValid(e.target.checked) }} />
+              <label htmlFor="">Number</label>
+            </div>
+            <div className="input-group ">
+              <input type="checkbox" name="Number" id="" onChange={(e) => { setCharValid(e.target.checked) }} />
+              <label htmlFor="">Character</label>
+            </div>
+            <div className="input-group">
+              <span onClick={handleReset} style={{ cursor: 'pointer', fontSize: '30px' }}><MdOutlineRestartAlt></MdOutlineRestartAlt>
+              </span>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
