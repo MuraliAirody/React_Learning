b7b86d3c7e4337c7bf21a8206d502cf6559f9cde
Anas Siddiqui <111227461+AnasSiddiqui18@users.noreply.github.com>
2023-12-27 14:20:31 +0500

    Update LogoutBtn.jsx

--- BlogApplication/src/component/Header/LogoutBtn.jsx
+++ BlogApplication/src/component/Header/LogoutBtn.jsx
@@ -1,24 +1,28 @@
-import React from 'react'
-
-import {useDispatch} from 'react-redux'
-import authService from '../../Appwrite/authService'
-import { logout } from '../../store/authSlice'
+import { useDispatch } from "react-redux";
+import authService from "../../Appwrite/authService";
+import { logout } from "../../store/authSlice";
+import { useNavigate } from "react-router-dom";
 
 function LogoutBtn() {
+  const navigate = useNavigate();
 
-  const dispatch = useDispatch()
+  const dispatch = useDispatch();
 
-  const logoutHandler = ()=>{
-    authService.logout()
-    .then(()=> dispatch(logout()))
-  }
+  const logoutHandler = async () => {
+    await authService
+      .logout()
+      .then(() => dispatch(logout()))
+      .then(navigate("/"));
+  };
 
   return (
     <button
-    className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
-    onClick={logoutHandler}
-    >Logout</button>
-  )
+      className="inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full"
+      onClick={logoutHandler}
+    >
+      Logout
+    </button>
+  );
 }
 
-export default LogoutBtn
+export default LogoutBtn;
